b8142424136a7f999bae2b20323365bd7c1aad41
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-10-23 08:48:47 -0700

    words

---
+++ Lab1022 WordSort/words.js
@@ -0,0 +1,20 @@
+var txt;
+function preload(){
+  // p5 has a function named "loadStrings"
+  txt = loadStrings("words.txt");
+}
+function setup() {
+}
+
+swap(nums, 2, 4)
+function swap(nums, a, b){
+   var temp = nums[a];
+   nums[a] = nums[b];
+   nums[b] = temp
+)
+for(var i = nums.length - 1; i >= 1; i-- ){
+    for(var j = 0; j < nums.length; j++ ){
+
+}
+
+}
