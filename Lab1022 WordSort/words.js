ce4755051b88f74bbcdcdb987467dfb320f865fd
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-10-24 08:49:17 -0700

    rg

--- Lab1022 WordSort/words.js
+++ Lab1022 WordSort/words.js
@@ -1,20 +1,18 @@
-var txt;
-function preload(){
-  // p5 has a function named "loadStrings"
+function setup(){
+  noCanvas();
+  var txt = []
   txt = loadStrings("words.txt");
 }
-function setup() {
+function draw() {
+  bubbleSort(txt);
 }
 
-swap(nums, 2, 4)
-function swap(nums, a, b){
-   var temp = nums[a];
-   nums[a] = nums[b];
-   nums[b] = temp
-)
-for(var i = nums.length - 1; i >= 1; i-- ){
-    for(var j = 0; j < nums.length; j++ ){
+console.log(txt);
+function bubbleSort(txt) {
+  var length = txt.length;
+  //the amount of passes
+  for (var i = 0; 1 < length; i++) {
+    // j < length
+    for (var j = 0; j < (length - i - 1); j++) {
 
-}
-
-}
+      
