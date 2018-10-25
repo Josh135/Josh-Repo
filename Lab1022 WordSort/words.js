6ab13cfc242ef1f11860138cb5774b3ca978ff5c
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-10-25 08:50:03 -0700

    dr

--- Lab1022 WordSort/words.js
+++ Lab1022 WordSort/words.js
@@ -1,18 +1,26 @@
-function setup(){
-  noCanvas();
-  var txt = []
-  txt = loadStrings("words.txt");
-}
-function draw() {
-  bubbleSort(txt);
-}
 
-console.log(txt);
-function bubbleSort(txt) {
-  var length = txt.length;
-  //the amount of passes
-  for (var i = 0; 1 < length; i++) {
-    // j < length
-    for (var j = 0; j < (length - i - 1); j++) {
+  var txt = [];
+  function preload(){
+    txt = loadStrings("words.txt");
+  }
+  function setup(){
+    noCanvas();
+    console.log(txt)
+    bubbleSort(txt);
+  }
 
-      
+  function draw() {
+  }
+  function bubbleSort(txt) {
+    var length = txt.length;
+    for (var i = 0; i < length; i++) {
+      for (var j = 0; j < (length - i - 1); j++) {
+        if(txt[j] > txt[j+1]) {
+          var temp = txt[j];
+          txt[j] = txt[j+1];
+          txt[j+1] = temp;
+          console.log(txt);
+        }
+      }
+    }
+  }
