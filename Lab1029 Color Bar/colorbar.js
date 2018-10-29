66abd8f0435960a7e40ae802d2fde4444273a3c3
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-10-29 08:49:37 -0700

    vgrf

---
+++ Lab1029 Color Bar/colorbar.js
@@ -0,0 +1,17 @@
+/*Josh Peterson
+Period 1
+Lab Color S0rt*/
+
+var bars = [];
+
+function setup() {
+  bubbleSort(txt);
+}
+
+function draw() {
+}
+
+function bubbleSort(bars) {
+  var length = bar.length;
+  for (var i = 0; i < length; i++) {
+    for (var j = 0; j < (length - i - 1)
