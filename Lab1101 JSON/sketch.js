1ed9f69487a60f1ce316d0596a956d601c9361b3
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-11-06 08:30:59 -0800

    znfs

--- Lab1101 JSON/sketch.js
+++ Lab1101 JSON/sketch.js
@@ -1,27 +1,36 @@
-var data;
-var barHeight;
-var txt;
-//  preload a large file
+var data = [];
 function preload(){
   data = loadJSON("population.json");
 }
-function setup(){
-  txt=data.objects;
 
+function setup() {
+  //remove blank black screen
+  noCanvas();
+  bubbleSort(data);
+  console.log(data.countrydata)
 }
-function bubbleSort(txt) {
-  var length = txt.length;
-  for (var i = 0; i < length; i++) {
-    for (var j = 0; j < (length - i - 1); j++) {
-      if(txt[j].countrydata.males > txt[j+1].countrydata.males) {
-        var temp = txt[j];
-        txt[j] = txt[j+1];
-        txt[j+1] = temp;
-      }
+
+function draw() {
+}
+
+function bubbleSort(data) {
+    var length = data.length;
+    //number of passes
+    for (var i = 0; i < length; i++) {
+        //notice that j < (length - i)
+        for (var j = 0; j < (length - i - 1); j++) {
+            //Compare the adjacent positions
+            var a = data[j].countrydata.country;
+            var b = data[j+1].countrydata.country;
+            if(data[j] > data[j+1]) {
+                //swap the numbers
+                //temperary variable
+                var temp = data[j];
+                //replace current string with adjacent string
+                data[j] = data[j+1];
+                //replace adjacent string with temp string (current string)
+                data[j+1] = temp;
+            }
+        }
     }
-  }
-}
-function draw(){
-  bubbleSort(txt);
-  console.log(txt);
 }
