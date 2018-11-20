893bc8b3533a2f2e72d6b5f81e7e1e8c1f5fd309
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-11-20 08:49:40 -0800

    fsf

--- Lab1114 SNAKEEE/sketch.js
+++ Lab1114 SNAKEEE/sketch.js
@@ -19,4 +19,13 @@ function keyPressed(){
   if(keyCode === UP_ARROW){
     snake.vel = createVector(0, -1);
   }
+  else if(keyCode === DOWN_ARROW){
+    snake.vel = createVector(0, 1);
+  }
+  else if(keyCode === LEFT_ARROW){
+    snake.vel = createVector(-1, 0);
+  }
+  else if(keyCode === RIGHT_ARROW){
+    snake.vel = createVector(1, 0);
+  }
 }
