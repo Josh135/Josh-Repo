8441fda1e8be43ac296f38e1cd201f93c3a42caf
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-11-26 08:48:54 -0800

    dg

--- Lab1114 SNAKEEE/sketch.js
+++ Lab1114 SNAKEEE/sketch.js
@@ -3,29 +3,30 @@ var cols, rows
 var snake;
 
 function setup(){
+  frameRate(10);
   var cnv = createCanvas(800, 800);
   cols = width/w;
   rows = height/w;
   cnv.position((windowWidth-width)/2, 30);
-  background(20, 20, 20);
   snake = new Snake(createVector(width/2, height/2), createVector(1, 0));
 }
 function draw(){
+  background(20, 20, 20);
   snake.run();
 }
 
 function keyPressed(){
-  text(keyCode, 50, 50);
+  text(keyCode, 0, 0);
   if(keyCode === UP_ARROW){
-    snake.vel = createVector(0, -1);
+    snake.vel = createVector(0, -1*w);
   }
   else if(keyCode === DOWN_ARROW){
-    snake.vel = createVector(0, 1);
+    snake.vel = createVector(0, 1*w);
   }
   else if(keyCode === LEFT_ARROW){
-    snake.vel = createVector(-1, 0);
+    snake.vel = createVector(-1*w, 0);
   }
   else if(keyCode === RIGHT_ARROW){
-    snake.vel = createVector(1, 0);
+    snake.vel = createVector(1*w, 0);
   }
 }
