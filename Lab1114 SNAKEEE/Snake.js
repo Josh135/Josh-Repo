e50fe167c08f8932015bc974c0311ad8cdddc0fb
Josh135 <43447572+Josh135@users.noreply.github.com>
2018-11-29 08:51:18 -0800

    dthj

--- Lab1114 SNAKEEE/Snake.js
+++ Lab1114 SNAKEEE/Snake.js
@@ -1,16 +1,17 @@
 /*Josh Peterson
 Period 1*/
+//snake function (allows snake to do what i say)
 function Snake(loc, vel){
   this.loc = loc;
   this.vel = vel;
   this.segments = [];
   this.segments.push(createVector(0, 0));
-
+//run function
   this.run = function(){
     this.update();
     this.render();
   }
-
+//update function
   this.update = function(){
     this.segments[0].x = this.loc.x;
     this.segments[0].x = this.loc.y;
@@ -18,7 +19,7 @@ function Snake(loc, vel){
     this.loc.x = constrain(this.loc.x, 0, 800-20)
     this.loc.y = constrain(this.loc.y, 0, 800-20)
   }
-
+//perameters for snake
   this.render = function(){
     noStroke();
     fill(0, 255, 0);
